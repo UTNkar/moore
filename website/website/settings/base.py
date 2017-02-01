0aa216c64d89e9276b164a8d7ab77b1435ffd08f
Jip J. Dekker <jip@dekker.li>
2017-02-01 16:54:35 +0100

    :lock: Upgrade hash algorithm to Argon2

--- website/website/settings/base.py
+++ website/website/settings/base.py
@@ -83,6 +83,17 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Password hashers
+# https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
+
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+]
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
