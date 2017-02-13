06139d26d8d6fd3174afed4dcd47a93824ae983d
Jip J. Dekker <jip@dekker.li>
2017-02-13 15:14:47 +0100

    Add user login page

--- website/website/urls.py
+++ website/website/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    # Static Pages
+    url(r'^$', TemplateView.as_view(template_name='base.html'), name='index'),
+    # Django Internal
+    url('^', include('django.contrib.auth.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^i18n/', include('django.conf.urls.i18n')),
 ]
