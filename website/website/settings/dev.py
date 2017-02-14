9a27cb143407aabe3dafa48f024e67696a0fec29
Jip J. Dekker <jip@dekker.li>
2017-02-14 12:21:41 +0100

    Merge wagtail settings with previous ones

--- website/website/settings/dev.py
+++ website/website/settings/dev.py
@@ -1,3 +1,12 @@
+"""
+Django settings for the development environment of Project Moore.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.10/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.10/ref/settings/
+"""
 from __future__ import absolute_import, unicode_literals
 
 from .base import *
@@ -8,6 +17,18 @@ DEBUG = True
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'l0gkw_q4rgex=0%ep84(yw$gx+mxr)u1e-x5yv@2j)a%(4=1!s'
 
+# Database
+# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# E-mail
+# https://docs.djangoproject.com/en/1.10/ref/settings/#email-backend
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
