a1eb8250c9bc47c07809bda0b1c5374f3a83eb2b
Jip J. Dekker <jip@dekker.li>
2017-02-16 13:22:01 +0100

    :speech_balloon: Make members verbose name translatable

--- website/members/apps.py
+++ website/members/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
 
 
 class MembersConfig(AppConfig):
     name = 'members'
-    verbose_name = 'UTN Member Management'
+    verbose_name = _('UTN Member Management')
