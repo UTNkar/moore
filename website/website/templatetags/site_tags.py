d4c35393dc0122bde022695109005b0dbd5cfe22
Jip J. Dekker <jip@dekker.li>
2017-02-21 16:03:33 +0100

    Add menu and breadcrumbs

--- website/website/templatetags/site_tags.py
+++ website/website/templatetags/site_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from wagtail.wagtailcore.models import Page
 
 register = template.Library()
 
@@ -8,3 +9,44 @@ def get_site_root(context):
     # NB this returns a core.Page, not the implementation-specific model used
     # so object-comparison to self will return false as objects would differ
     return context['request'].site.root_page
+
+
+def has_menu_children(page):
+    return page.get_children().live().in_menu().exists()
+
+
+# Retrieves the top menu items - the immediate children of the parent page
+# The has_menu_children method is necessary because the bootstrap menu requires
+# a dropdown class to be applied to a parent
+@register.inclusion_tag('tags/main_menu.html', takes_context=True)
+def main_menu(context, parent, calling_page=None):
+    menuitems = parent.get_children().live().in_menu()
+    for menuitem in menuitems:
+        menuitem.show_dropdown = has_menu_children(menuitem)
+        if menuitem.recruitmentpage:
+            menuitem.translated_title = menuitem.recruitmentpage\
+                .translated_title
+        # We don't directly check if calling_page is None since the template
+        # engine can pass an empty string to calling_page
+        # if the variable passed as calling_page does not exist.
+        menuitem.active = (calling_page.url.startswith(menuitem.url)
+                           if calling_page else False)
+    return {
+        'calling_page': calling_page,
+        'menuitems': menuitems,
+        # required by the pageurl tag that we want to use within this template
+        'request': context['request'],
+    }
+
+
+# Retrieves the children of the top menu items for the drop downs
+@register.inclusion_tag('tags/main_menu_children.html', takes_context=True)
+def main_menu_children(context, parent):
+    menuitems_children = parent.get_children()
+    menuitems_children = menuitems_children.live().in_menu()
+    return {
+        'parent': parent,
+        'menuitems_children': menuitems_children,
+        # required by the pageurl tag that we want to use within this template
+        'request': context['request'],
+    }
