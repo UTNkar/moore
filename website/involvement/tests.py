c9d055713926778718fae510ae3d4766e6ad7192
Jip J. Dekker <jip@dekker.li>
2017-03-08 11:26:48 +0100

    :white_check_mark: Add first tests for RecruitmentPage

--- website/involvement/tests.py
+++ website/involvement/tests.py
@@ -1,3 +1,168 @@
-from django.test import TestCase
+from datetime import date, timedelta
 
-# Create your tests here.
+from django.contrib.auth.models import Group
+from wagtail.tests.utils import WagtailPageTests
+from wagtail.wagtailcore.models import Page
+
+from home.models import HomePage
+from involvement.models import RecruitmentPage, Position, Function, Team
+
+
+class RecruitmentPageTests(WagtailPageTests):
+    def setUp(self):
+        self.login()
+
+        parent = Page.objects.get(url_path='/home/')
+        self.page = RecruitmentPage(
+            title='Apply now!',
+            title_sv='Ansök nu!',
+            slug='apply'
+        )
+        parent.add_child(instance=self.page)
+
+    def test_can_create_page(self):
+        parent = Page.get_root_nodes()[0]
+
+        # Assert that a RecruitmentPage can be made here, with this POST data
+        self.assertCanCreate(parent, RecruitmentPage, {
+            'title': 'Apply now!',
+            'title_sv': 'Ansök nu!',
+            'slug': 'apply2',
+        }, 'Recruitment pages can be created at the root')
+
+    def test_can_create_under_home_page(self):
+        """
+        It is possible to create a recruitment page with a home page as its
+        parent.
+        """
+        self.assertCanCreateAt(HomePage, RecruitmentPage)
+
+    def test_open_positions_empty(self):
+        """
+        If there are no open positions, an appropriate message should be
+        displayed.
+        """
+        response = self.client.get(
+            self.page.url + self.page.reverse_subpage('open_positions')
+        )
+
+        self.assertEqual(response.status_code, 200,
+                         msg='You can open the positions page')
+        self.assertContains(response, 'There are currently no open positions. '
+                                      'Check back later!')
+        self.assertQuerysetEqual(response.context['positions'], [],
+                                 msg='Response context does not contain'
+                                     ' positions')
+
+    def test_open_positions_one_position(self):
+        """
+        When there are open positions, these positions should be visible and
+        a link should be available to their specific page.
+        """
+        function = Function.objects.create(
+            name_en='Test Function',
+            name_sv='Test Funktion',
+        )
+        position = Position.objects.create(
+            function=function,
+            commencement=date.today() - timedelta(days=1),
+            deadline=date.today(),
+            term_from=date.today() + timedelta(days=1),
+            term_to=date.today() + timedelta(days=365),
+        )
+        response = self.client.get(
+            self.page.url + self.page.reverse_subpage('open_positions')
+        )
+
+        self.assertEqual(response.status_code, 200,
+                         'You can open the positions page')
+        self.assertContains(response, position.__str__(),
+                            msg_prefix='Response contains position name')
+        self.assertContains(
+            response,
+            self.page.url + self.page.reverse_subpage(
+                'position',
+                [position.id]
+            ),
+            msg_prefix='Response contains position URL'
+        )
+        self.assertQuerysetEqual(response.context['positions'],
+                                 ['<Position: ' + position.__str__() + '>'],
+                                 msg='Response context contains the position')
+
+    def test_position_view_general(self):
+        function = Function.objects.create(
+            name_en='Test Function',
+            name_sv='Test Funktion',
+        )
+        position = Position.objects.create(
+            function=function,
+            commencement=date.today() - timedelta(days=1),
+            deadline=date.today(),
+            term_from=date.today() + timedelta(days=1),
+            term_to=date.today() + timedelta(days=365),
+        )
+        response = self.client.get(self.page.url + self.page.reverse_subpage(
+            'position',
+            [position.id]
+        ))
+
+        self.assertEqual(response.status_code, 200,
+                         msg='You can open the position page')
+        self.assertContains(response, position.__str__(),
+                            msg_prefix='Response contains position name')
+        self.assertEqual(response.context['position'],
+                         position,
+                         msg='Response context contains the position')
+
+    def test_position_view_unaffiliated(self):
+        function = Function.objects.create(
+            team=None,
+            name_en='Test Function',
+            name_sv='Test Funktion',
+        )
+        position = Position.objects.create(
+            function=function,
+            commencement=date.today() - timedelta(days=1),
+            deadline=date.today(),
+            term_from=date.today() + timedelta(days=1),
+            term_to=date.today() + timedelta(days=365),
+        )
+        response = self.client.get(self.page.url + self.page.reverse_subpage(
+            'position',
+            [position.id]
+        ))
+
+        self.assertContains(response, 'Unaffiliated',
+                            msg_prefix='Position is listed as unaffiliated')
+
+    def test_position_view_team(self):
+        team = Team.objects.create(
+            name_en='Test Team',
+            name_sv='Testteam',
+            email='test@localhost',
+            group=Group.objects.first()
+        )
+        function = Function.objects.create(
+            team=team,
+            name_en='Test Function',
+            name_sv='Test Funktion',
+        )
+        position = Position.objects.create(
+            function=function,
+            commencement=date.today() - timedelta(days=1),
+            deadline=date.today(),
+            term_from=date.today() + timedelta(days=1),
+            term_to=date.today() + timedelta(days=365),
+        )
+        response = self.client.get(self.page.url + self.page.reverse_subpage(
+            'position',
+            [position.id]
+        ))
+
+        self.assertContains(response, team.name,
+                            msg_prefix='Position is listed as part of team')
+        self.assertContains(response, team.email,
+                            msg_prefix='Team contact information is provided')
+
+    # TODO: Add tests for my_positions
