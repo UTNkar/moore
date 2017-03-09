98b6cf4176a8f2fb75b500b6b30400b141bba4fd
Jip J. Dekker <jip@dekker.li>
2017-03-09 23:30:36 +0100

    :art: Disable buttons until changes occur

--- website/website/static/js/materialist.js
+++ website/website/static/js/materialist.js
@@ -148,6 +148,7 @@ $(document).ready(function() {
 		});
 	});
 
+	// Committee/Section header
 	$('#section-switch').click(function(ev) {
 	    ev.preventDefault();
 	    $('.committees').hide();
@@ -158,4 +159,22 @@ $(document).ready(function() {
 	    $('.sections').hide();
 	    $('.committees').show();
     });
+
+	// Misc
+	$('body').append('<div id="toasty-darth"><img src="/static/libraries/misc/darth.png" alt="toasty"></div>');
+	$('#toasty-darth').css('position', 'fixed');
+	$('#toasty-darth').css('right', '-200px');
+	$('#toasty-darth').css('bottom', '0px');
+	$('body').append('<audio id="toasty-audio"><source src="/static/libraries/misc/nooo.mp3" type="audio/mpeg"></audio>');
+	$('.disabled').click(function(ev) {
+		if(!$(ev.target).hasClass('disabled')) {
+			return;
+		}
+		ev.preventDefault();
+		console.log(ev);
+		var audio = document.getElementById('toasty-audio');
+		audio.play();
+		$("#toasty-darth").addClass("show-darth");
+		setTimeout(function(){ $("#toasty-darth").removeClass("show-darth"); }, 5000);
+	});
 });
