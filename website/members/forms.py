e68b35bbe70457befa9121cfadce7156454e3fda
Jip J. Dekker <jip@dekker.li>
2017-03-10 14:39:56 +0100

    :art: Add account settings

--- website/members/forms.py
+++ website/members/forms.py
@@ -1,12 +1,64 @@
 from django import forms
+from django.core import validators
+from django.forms import ModelForm, TextInput, Select
 from django.utils.translation import ugettext_lazy as _
 
 from wagtail.wagtailusers.forms import UserEditForm, UserCreationForm
 
-from members.models import StudyProgram
+from members.models import StudyProgram, Member
+
+
+class MemberForm(ModelForm):
+    person_number = forms.CharField(
+        max_length=13,
+        min_length=12,
+        help_text=_('Person number using the YYYYMMDD-XXXX format.'),
+        validators=[validators.RegexValidator(
+            regex=r'^\d{8}\-?\d{4}$',
+            message=_('Use the format YYYYMMDD-XXXX for your person number.')
+        )],
+        widget=TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'YYYYMMDD-XXXX',
+        })
+    )
+
+    class Meta:
+        model = Member
+        fields = ['first_name', 'last_name', 'phone_number',
+                  'registration_year', 'study']
+        widgets = {
+            'first_name': TextInput(attrs={'class': 'form-control'}),
+            'last_name': TextInput(attrs={'class': 'form-control'}),
+            'phone_number': TextInput(attrs={'class': 'form-control'}),
+            'registration_year': TextInput(attrs={'class': 'form-control'}),
+            'study': Select(attrs={'class': 'form-control'}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        instance = kwargs.get('instance', None)
+
+        if instance is not None:
+            kwargs.update(initial={
+                'person_number': instance.person_number()
+            })
+
+        super(ModelForm, self).__init__(*args, **kwargs)
+
+    def save(self, commit=True):
+        person_number = self.cleaned_data['person_number']
+        self.instance.birthday = '%s-%s-%s' % (
+            person_number[:4], person_number[4:6], person_number[6:8]
+        )
+        self.instance.person_number_ext = person_number[-4:]
+
+        return super(ModelForm, self).save(commit=commit)
 
 
 class CustomUserEditForm(UserEditForm):
+    """
+    Custom form to edit users from within the wagtail admin interface.
+    """
     birthday = forms.DateField(required=True, label=_('Birthday'))
     person_number_ext = forms.CharField(
         required=True,
@@ -28,6 +80,9 @@ class CustomUserEditForm(UserEditForm):
 
 
 class CustomUserCreationForm(UserCreationForm):
+    """
+    Custom form to create user from within the Wagtail admin user interface.
+    """
     birthday = forms.DateField(required=True, label=_('Birthday'))
     person_number_ext = forms.CharField(
         required=True,
--- website/members/views.py
+++ website/members/views.py
@@ -1,3 +1,25 @@
-# from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+from django.utils.translation import ugettext_lazy as _
 
-# Create your views here.
+from members.forms import MemberForm
+
+
+@login_required
+def profile(request):
+    page = {'title': _('Account Settings')}
+    saved = False
+
+    if request.POST:
+        form = MemberForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            saved = True
+    else:
+        form = MemberForm(instance=request.user)
+
+    return render(request, 'members/profile.html', {
+        'saved': saved,
+        'page': page,
+        'form': form,
+    })
