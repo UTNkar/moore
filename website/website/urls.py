9cf81df09d1daac155641e4ada26bd79263c8dbb
Jip J. Dekker <jip@dekker.li>
2017-03-14 16:12:07 +0100

    :fire: Disable Django Admin

--- website/website/urls.py
+++ website/website/urls.py
@@ -2,7 +2,6 @@ from __future__ import absolute_import, unicode_literals
 
 from django.conf import settings
 from django.conf.urls import include, url
-from django.contrib import admin
 
 from search import views as search_views
 from wagtail.wagtailadmin import urls as wagtailadmin_urls
@@ -10,9 +9,6 @@ from wagtail.wagtailcore import urls as wagtail_urls
 from wagtail.wagtaildocs import urls as wagtaildocs_urls
 
 urlpatterns = [
-    # TODO: Disable django admin
-    url(r'^django-admin/', include(admin.site.urls)),
-
     url(r'^admin/', include(wagtailadmin_urls)),
     url(r'^documents/', include(wagtaildocs_urls)),
 
