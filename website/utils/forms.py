9e4cd930815b50aee01e56d7b815bbcff5726a91
Jip J. Dekker <jip@dekker.li>
2017-03-20 11:06:56 +0100

    :memo: Add documentation to Advanced Model Fields

--- website/utils/forms.py
+++ website/utils/forms.py
@@ -1,8 +1,18 @@
+"""
+ModelChoiceFields that have access to the actual objects, not just their string
+value. Inspired by the following blog post:
+http://srcmvn.com/blog/2013/01/15/django-advanced-model-choice-field/
+"""
+
 from django.forms import models
 from django.forms.fields import ChoiceField
 
 
 class AdvancedModelChoiceIterator(models.ModelChoiceIterator):
+    """
+    Iterator user by Advanced[Multiple]ModelChoice fields. It adds the object
+    itself as an additional argument to the choices
+    """
     def choice(self, obj):
         return (
             self.field.prepare_value(obj),
@@ -12,6 +22,10 @@ class AdvancedModelChoiceIterator(models.ModelChoiceIterator):
 
 
 class AdvancedModelChoiceField(models.ModelChoiceField):
+    """
+    An extension of ModelChoiceField that contains the object itself within its
+    choices.
+    """
     def _get_choices(self):
         if hasattr(self, '_choices'):
             return self._choices
@@ -22,6 +36,10 @@ class AdvancedModelChoiceField(models.ModelChoiceField):
 
 
 class AdvancedModelMultipleChoiceField(models.ModelMultipleChoiceField):
+    """
+    An extension of ModelMultipleChoiceField that contains the object itself
+    within its choices.
+    """
     def _get_choices(self):
         if hasattr(self, '_choices'):
             return self._choices
