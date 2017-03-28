f2b9446544b8b62f177d482947ab20ac523ea4ae
Jip J. Dekker <jip@dekker.li>
2017-03-28 15:06:35 +0200

    Check membership when a Member is saved

--- website/members/signals.py
+++ website/members/signals.py
@@ -1,13 +1,18 @@
+import datetime
 from django.conf import settings
 from django.core.mail import EmailMultiAlternatives
+from django.db.models.signals import pre_save
 from django.dispatch import receiver
 from django.template import loader
+from django.utils import timezone
 from simple_email_confirmation import unconfirmed_email_created
 
+from members.models import Member
 
-@receiver(unconfirmed_email_created,  dispatch_uid='send_email_confirmation')
+
+@receiver(unconfirmed_email_created, dispatch_uid='send_email_confirmation')
 def send_confirmation_email(sender, email, user=None, **kwargs):
-    member = user or sender
+    member = user
     context = {
         'email': email,
         'domain': settings.BASE_URL,
@@ -24,3 +29,9 @@ def send_confirmation_email(sender, email, user=None, **kwargs):
 
     email_message = EmailMultiAlternatives(subject, body, None, [email])
     email_message.send()
+
+
+@receiver(pre_save, sender=Member, dispatch_uid='member_check_membership')
+def check_membership(sender, instance, **kwargs):
+    if timezone.now() - instance.status_changed > datetime.timedelta(1):
+        instance.update_status()
