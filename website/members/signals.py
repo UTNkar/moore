3cf376d57c5cd535504105d9ee62ca3512dbac13
Jip J. Dekker <jip@dekker.li>
2017-03-29 14:20:13 +0200

    :bug: :green_heart: Disable confirmation e-mails for None

--- website/members/signals.py
+++ website/members/signals.py
@@ -12,23 +12,23 @@ from members.models import Member
 
 @receiver(unconfirmed_email_created, dispatch_uid='send_email_confirmation')
 def send_confirmation_email(sender, email, user=None, **kwargs):
-    member = user
-    context = {
-        'email': email,
-        'domain': settings.BASE_URL,
-        'site_name': settings.WAGTAIL_SITE_NAME,
-        'token': member.get_confirmation_key(email),
-    }
+    if user is not None:
+        context = {
+            'email': email,
+            'domain': settings.BASE_URL,
+            'site_name': settings.WAGTAIL_SITE_NAME,
+            'token': user.get_confirmation_key(email),
+        }
 
-    subject = loader.render_to_string(
-        'members/email_change_subject.txt', context)
-    # Email subject *must not* contain newlines
-    subject = ''.join(subject.splitlines())
-    body = loader.render_to_string('members/email_change_email.html',
-                                   context)
+        subject = loader.render_to_string(
+            'members/email_change_subject.txt', context)
+        # Email subject *must not* contain newlines
+        subject = ''.join(subject.splitlines())
+        body = loader.render_to_string('members/email_change_email.html',
+                                       context)
 
-    email_message = EmailMultiAlternatives(subject, body, None, [email])
-    email_message.send()
+        email_message = EmailMultiAlternatives(subject, body, None, [email])
+        email_message.send()
 
 
 @receiver(pre_save, sender=Member, dispatch_uid='member_check_membership')
