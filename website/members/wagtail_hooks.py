8ec110505aa2726cfb0ff6a24186359be9f08e3d
Jip J. Dekker <jip@dekker.li>
2017-04-04 11:33:05 +0200

    Add model for sections

--- website/members/wagtail_hooks.py
+++ website/members/wagtail_hooks.py
@@ -1,10 +1,12 @@
-from wagtail.contrib.modeladmin.options import ModelAdmin, modeladmin_register
-from members.models import StudyProgram
+from wagtail.contrib.modeladmin.options import ModelAdmin, ModelAdminGroup, \
+    modeladmin_register
+from members.models import StudyProgram, Section
+from django.utils.translation import ugettext_lazy as _
 
 
 class StudyProgramAdmin(ModelAdmin):
     model = StudyProgram
-    menu_icon = 'bold'  # TODO: Is there a better option?
+    menu_icon = 'fa-graduation-cap'
     menu_order = 510
     add_to_settings_menu = False
     list_display = ('degree', 'name_en', 'name_sv')
@@ -12,4 +14,19 @@ class StudyProgramAdmin(ModelAdmin):
                      'abbreviation_sv')
 
 
-modeladmin_register(StudyProgramAdmin)
+class SectionAdmin(ModelAdmin):
+    model = Section
+    menu_icon = 'fa-eye'
+    menu_order = 520
+    add_to_settings_menu = False
+    list_display = ('abbreviation', 'name_en', 'name_sv')
+    search_fields = ('name_en', 'name_sv', 'abbreviation')
+
+
+class EducationAdminGroup(ModelAdminGroup):
+    menu_label = _('Education')
+    menu_icon = 'fa-university'
+    menu_order = 450
+    items = (StudyProgramAdmin, SectionAdmin)
+
+modeladmin_register(EducationAdminGroup)
