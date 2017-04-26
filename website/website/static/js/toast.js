b52eff1094d77c050f7205eb0a4835f132c8ab3d
Jip J. Dekker <jip@dekker.li>
2017-04-26 11:08:51 +0200

    Add colors to toast messages

--- website/website/static/js/toast.js
+++ website/website/static/js/toast.js
@@ -1,11 +1,20 @@
 $(document).ready(function() {
-    $('.message').each(function( index ) {
+    $('.message').each(function(index, value) {
+        var classes = 'rounded';
+        if ($(value).hasClass('error')) {
+            classes += ' red';
+        } else if ($(value).hasClass('warning')) {
+            classes += ' yellow';
+        }
         Materialize.toast(
             '<div class="message-impl">' +
             // TODO: Replace this static reference.
             '<img class="marvin" src="/static/images/marvin.png">' +
             '<b>Marvin:</b><br>'
             + $(this).text()
-            + '</div>');
+            + '</div>',
+            null, // Display until dismissed
+            classes
+        );
     });
 });
