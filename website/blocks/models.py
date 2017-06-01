2be966b078461109b7f68ec8c20680222c1e6f13
Jip J. Dekker <jip@dekker.one>
2017-06-01 14:51:15 +0200

    Add Image+Icons block

--- website/blocks/models.py
+++ website/blocks/models.py
@@ -20,6 +20,7 @@ class CountersBlock(blocks.StructBlock):
     ])
 
     class Meta:
+        label = _('Counters')
         icon = 'fa-balance-scale'
         template = 'blocks/counter.html'
 
@@ -29,6 +30,7 @@ class HeadingBlock(blocks.StructBlock):
     subtitle = blocks.CharBlock(required=False)
 
     class Meta:
+        label = _('Heading')
         icon = 'fa-header'
         template = 'blocks/title.html'
 
@@ -43,13 +45,38 @@ class ImageDescription(blocks.StructBlock):
     hide_on_med = blocks.BooleanBlock(required=False)
 
     class Meta:
-        icon = 'fa-picture-o'
+        label = _('Image + Description')
+        icon = 'fa-file-image-o '
         template = 'blocks/image_description.html'
+
+
+class ImageIcons(blocks.StructBlock):
+    title = blocks.CharBlock()
+    image = ImageChooserBlock()
+    image_alignment = blocks.ChoiceBlock(choices=[
+        ('left', _('Left')),
+        ('right', _('Right')),
+    ])
+    icons = blocks.ListBlock(blocks.StructBlock([
+        ('icon', blocks.CharBlock(
+            help_text=_('Material icon font icon text, as found on: '
+                        'https://material.io/icons'),
+        )),
+        ('title', blocks.CharBlock()),
+        ('description', blocks.CharBlock())
+    ]))
+    hide_on_med = blocks.BooleanBlock(required=False)
+
+    class Meta:
+        label = _('Image + Icons')
+        icon = 'fa-file-excel-o'
+        template = 'blocks/image_icons.html'
 
 WAGTAIL_CONTENT_BLOCKTYPES = [
     ('heading', HeadingBlock()),
     ('paragraph', blocks.RichTextBlock()),
-    ('image_description', ImageDescription()),
+    ('image_description', ImageIcons()),
+    ('image_icons', ImageDescription()),
     ('counters', CountersBlock()),
     ('image', ImageChooserBlock(template='blocks/image.html')),
 ]
