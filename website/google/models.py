b89a4f064fb7a6285bf8e2b74f80204add1feff5
Jip J. Dekker <jip@dekker.one>
2017-06-14 14:01:26 +0200

    Add block for Google Drive

--- website/google/models.py
+++ website/google/models.py
@@ -11,3 +11,17 @@ class GoogleFormBlock(blocks.StructBlock):
         label = _('Google Form')
         icon = 'fa-check-square-o'
         template = 'google/blocks/form.html'
+
+
+class GoogleDriveBlock(blocks.StructBlock):
+    folder_id = blocks.CharBlock()
+    view = blocks.ChoiceBlock(choices=[
+        ('list', _('List')),
+        ('grid', _('Grid')),
+    ])
+    height = blocks.IntegerBlock()
+
+    class Meta:
+        label = _('Google Drive')
+        icon = 'fa-folder-open'
+        template = 'google/blocks/drive.html'
