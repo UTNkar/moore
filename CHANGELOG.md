8d96a214c7894c725f3bae4d984b7a7e16e708cd
Jip J. Dekker <jip@dekker.one>
2017-06-30 15:26:35 +0200

    :bug: Fix the translation of submenus

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,8 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
 ## [Unreleased]
+### Fixed
+- Translation of sub-menus
 
 ## [0.5.1]
 ### Added
--- website/website/templatetags/site_tags.py
+++ website/website/templatetags/site_tags.py
@@ -20,26 +20,9 @@ def has_menu_children(page):
 @register.inclusion_tag('tags/menu.html', takes_context=True)
 def menu_items(context, parent, calling_page=None, sidenav=False):
     menuitems = parent.get_children().live().in_menu()
+    menuitems = [m.specific for m in menuitems]
     for menuitem in menuitems:
         menuitem.show_dropdown = has_menu_children(menuitem)
-        # TODO: There has to be a better alternative!
-        if hasattr(menuitem, 'googleformindex'):
-            menuitem.translated_title = menuitem.googleformindex\
-                .translated_title
-        elif hasattr(menuitem, 'googleformpage'):
-            menuitem.translated_title = menuitem.googleformpage\
-                .translated_title
-        elif hasattr(menuitem, 'homepage'):
-            menuitem.translated_title = menuitem.homepage.translated_title
-        elif hasattr(menuitem, 'recruitmentpage'):
-            menuitem.translated_title = menuitem.recruitmentpage\
-                .translated_title
-        elif hasattr(menuitem, 'newsindexpage'):
-            menuitem.translated_title = menuitem.newsindexpage.translated_title
-        elif hasattr(menuitem, 'newspage'):
-            menuitem.translated_title = menuitem.newspage.translated_title
-        elif hasattr(menuitem, 'webpage'):
-            menuitem.translated_title = menuitem.webpage.translated_title
         # We don't directly check if calling_page is None since the template
         # engine can pass an empty string to calling_page
         # if the variable passed as calling_page does not exist.
@@ -57,8 +40,8 @@ def menu_items(context, parent, calling_page=None, sidenav=False):
 # Retrieves the children of the top menu items for the drop downs
 @register.inclusion_tag('tags/menu_children.html', takes_context=True)
 def menu_children(context, parent, sidenav=False):
-    children = parent.get_children()
-    children = children.live().in_menu()
+    children = parent.get_children().live().in_menu()
+    children = [c.specific for c in children]
     return {
         'parent': parent,
         'children': children,
