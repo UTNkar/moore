f40f896f56e05d5d35ef47b574ffbbc53e57ab34
Jip J. Dekker <jip@dekker.one>
2017-07-18 09:51:57 +0200

    :triangular_ruler: Rename folder structure to better reflect functionality
    outer website => src, as it contains the django project
    inner website => moore, containing the project settings
    and project related files

--- src/moore/wsgi.py
+++ src/moore/wsgi.py
@@ -14,6 +14,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "moore.settings.dev")
 
 application = Sentry(get_wsgi_application())
