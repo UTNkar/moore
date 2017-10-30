237ff558d37b99ed5ba7c490fe036986dd751dec
Jip J. Dekker <jip@dekker.one>
2017-10-30 17:20:31 +1100

    :bug: Fix the inaccessible pages within the involvement package

--- src/moore/urls.py
+++ src/moore/urls.py
@@ -9,8 +9,8 @@ from wagtail.wagtailcore import urls as wagtail_urls
 from wagtail.wagtaildocs import urls as wagtaildocs_urls
 
 urlpatterns = [
+    url(r'', include('involvement.urls')),  # Needs to be imported before wagtail admin
     url(r'^admin/', include(wagtailadmin_urls)),
-    url(r'', include('involvement.urls')),
     url(r'^documents/', include(wagtaildocs_urls)),
 
     url(r'^search/$', search_views.search, name='search'),
