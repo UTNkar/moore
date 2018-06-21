8616e3d4a814b98f22d8ffed2d0f483a13602a96
furedal <cfuredal@gmail.com>
2018-06-21 13:11:00 +0200

    refactor rules depends on what team they are connected to

--- src/involvement/rules.py
+++ src/involvement/rules.py
@@ -1,16 +1,25 @@
 import rules
 from involvement.models import Role
-from involvement.rule_utils import is_admin, is_fum, has_permission, \
-            has_role_perm
+from involvement.rule_utils import is_super, is_admin, is_fum, has_role_perm
 
 
 # Application predicates
+@rules.predicate
+def member_of_team_appl(user, application):
+    return member_of_team_role(user, application.position.role)
+
+
 @rules.predicate
 def can_modify_application(user, application):
     return can_set_applicant(user, application.position)
 
 
 # Position predicates
+@rules.predicate
+def member_of_team_position(user, position):
+    return member_of_team_role(user, position.role)
+
+
 @rules.predicate
 def is_action_approve(user, position):
     return position.current_action() == 'approve'
@@ -23,8 +32,7 @@ def is_action_appoint(user, position):
 
 @rules.predicate
 def can_set_applicant(user, position):
-    permission_filter = Role.edit_applicant_codenames(user)
-    return has_permission(position.role.group.permissions, permission_filter)
+    return position.role.role_type in Role.edit_applicant_role_types(user)
 
 
 @rules.predicate
@@ -35,38 +43,55 @@ def can_modify_position(user, position):
 # Role predicates
 @rules.predicate
 def can_modify_role(user, role):
-    permission_filter = Role.editable_codenames(user)
-    return has_permission(role.group.permissions, permission_filter)
+    return role.role_type in Role.editable_role_types(user)
+
+
+@rules.predicate
+def member_of_team_role(user, role):
+    return not user.is_anonymous and user.teams.filter(pk__in=role.teams.values_list('pk', flat=True))
+
+
+# Team predicates
+@rules.predicate
+def member_of_team(user, team):
+    return not user.is_anonymous and user.teams.filter(pk=team.pk).exists()
 
 
 # Permissions
 rules.add_perm('involvement', rules.always_allow)
 
-rules.add_perm('involvement.list_application', is_admin | has_role_perm)
-rules.add_perm('involvement.add_application', is_admin | has_role_perm)
-rules.add_perm('involvement.change_application', is_admin
-               | can_modify_application)
-rules.add_perm('involvement.delete_application', is_admin
-               | can_modify_application)
+rules.add_perm('involvement.list_application', is_super | has_role_perm)
+rules.add_perm('involvement.add_application', is_super | has_role_perm)
+rules.add_perm('involvement.change_application', is_super
+               | member_of_team_appl & can_modify_application)
+rules.add_perm('involvement.delete_application', is_super
+               | member_of_team_appl & can_modify_application)
 
-rules.add_perm('involvement.list_position', is_admin | has_role_perm)
-rules.add_perm('involvement.inspect_position', is_admin | has_role_perm)
-rules.add_perm('involvement.add_position', is_admin | has_role_perm)
-rules.add_perm('involvement.change_position', is_admin | can_modify_position)
-rules.add_perm('involvement.delete_position', is_admin | can_modify_position)
+rules.add_perm('involvement.list_position', is_super | has_role_perm)
+rules.add_perm('involvement.add_position', is_super | has_role_perm)
+rules.add_perm('involvement.inspect_position', is_super
+               | member_of_team_position & has_role_perm)
+rules.add_perm('involvement.change_position', is_super
+               | member_of_team_position & can_modify_position)
+rules.add_perm('involvement.delete_position', is_super | (member_of_team_position & can_modify_position))
 
-rules.add_perm('involvement.approve_position', is_admin
-               | (is_action_approve & can_modify_position))
-rules.add_perm('involvement.appoint_position', is_admin
-               | (is_action_appoint & ~is_fum & can_set_applicant))
+rules.add_perm('involvement.approve_position', is_super
+               | (member_of_team_position & is_action_approve & can_modify_position))
+rules.add_perm('involvement.appoint_position', is_super
+               | (member_of_team_position & is_action_appoint & ~is_fum & can_set_applicant))
 
-rules.add_perm('involvement.list_role', is_admin | has_role_perm)
-rules.add_perm('involvement.add_role', is_admin | has_role_perm)
-rules.add_perm('involvement.change_role', is_admin | can_modify_role)
-rules.add_perm('involvement.delete_role', is_admin | can_modify_role)
+rules.add_perm('involvement.list_role', is_super | has_role_perm)
+rules.add_perm('involvement.add_role', is_super | has_role_perm)
+rules.add_perm('involvement.change_role', is_super
+               | member_of_team_role & can_modify_role)
+rules.add_perm('involvement.delete_role', is_super
+               | member_of_team_role & can_modify_role)
 
-rules.add_perm('involvement.list_team', is_admin)
-rules.add_perm('involvement.inspect_team', is_admin)
-rules.add_perm('involvement.add_team', is_admin)
-rules.add_perm('involvement.change_team', is_admin)
-rules.add_perm('involvement.delete_team', is_admin)
+rules.add_perm('involvement.list_team', is_super | is_admin)
+rules.add_perm('involvement.add_team', is_super | is_admin)
+rules.add_perm('involvement.inspect_team', is_super
+               | member_of_team & is_admin)
+rules.add_perm('involvement.change_team', is_super
+               | member_of_team & is_admin)
+rules.add_perm('involvement.delete_team', is_super
+               | member_of_team & is_admin)
