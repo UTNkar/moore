0305c2ba7c06f092b23640275a8fd0585ecaa2e6
furedal <cfuredal@gmail.com>
2018-06-21 13:11:44 +0200

    update sync_group to handle role->group connection

--- src/involvement/cron.py
+++ src/involvement/cron.py
@@ -5,19 +5,18 @@ from django.conf import settings
 from django.core.mail import EmailMultiAlternatives
 from django.template import loader
 
-from involvement.models import Team, Application, Position
+from involvement.models import Team, Role, Application, Position
 
 
 @kronos.register('1 0,6,12,18 * * *')  # At minute 1 past hour 0, 6, 12, and 18
 def sync_groups():
-    for team in Team.objects.all():
+    for role in Role.objects.all():
         # Add new members
-        for member in team.get_members():
-            if team.group not in member.groups.all():
-                team.group.user_set.add(member)
-
+        for member in role.members:
+            if role.group not in member.groups.all():
+                role.group.user_set.add(member)
                 applications = member.application_set.filter(
-                    position__role__team=team,
+                    position__role=role,
                     position__term_from__lte=date.today(),
                     position__term_to__gte=date.today(),
                     status='appointed',
@@ -28,14 +27,14 @@ def sync_groups():
 
         # Remove old members
         old_members = Application.objects.filter(
-            position__role__team=team,
+            position__role=role,
             position__term_to__lt=date.today(),
             status='appointed',
             removed=False,
         )
         for member in old_members:
-            if team.group in member.applicant.groups.all():
-                team.group.user_set.remove(member.applicant)
+            if role.group in member.applicant.groups.all():
+                role.group.user_set.remove(member.applicant)
             member.removed = True
             member.save()
 
