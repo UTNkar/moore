2590334b532d288344457dc99ead457646abda0c
furedal <cfuredal@gmail.com>
2018-06-25 11:34:56 +0200

    refactor method name

--- src/involvement/views/position_inspect_view.py
+++ src/involvement/views/position_inspect_view.py
@@ -4,11 +4,11 @@ from wagtail.contrib.modeladmin.views import InspectView
 class PositionInspectView(InspectView):
     def get_context_data(self, **kwargs):
         context = super(PositionInspectView, self).get_context_data(**kwargs)
-        context['appointed_applications'] = self.get_current_applications
+        context['appointed_applications'] = self.get_appointed_applications
         context['submitted_applications'] = self.get_submitted_applications
         return context
 
-    def get_current_applications(self):
+    def get_appointed_applications(self):
         return self.instance.applications.filter(
             status='appointed'
         )
