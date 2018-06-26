d2f0eb7fbabc41d19a8e26a4312d000b5b132bb7
furedal <cfuredal@gmail.com>
2018-06-26 14:07:47 +0200

    refactor contact block to reference application instead of role.

--- src/involvement/models/contact_card.py
+++ src/involvement/models/contact_card.py
@@ -1,21 +1,58 @@
+from datetime import date
+from django import forms
+from django.apps import apps
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.admin.forms import WagtailAdminModelForm
 from wagtail.core.fields import RichTextField
 from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.snippets.models import register_snippet
 from utils.translation import TranslatedField
 
 
+class ContactBlockForm(WagtailAdminModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(ContactBlockForm, self).__init__(*args, **kwargs)
+        contact_card = self.instance
+        Application = apps.get_model('involvement', 'Application')
+
+        if contact_card.application:
+            self.fields['application'].queryset = Application.objects.filter(
+                models.Q(
+                    position__term_from__lte=date.today(),
+                    position__term_to__gte=date.today(),
+                    status='appointed',
+                    contact_card__isnull=True,
+                ) | # Keep selected application in list
+                models.Q(
+                    position__term_from__lte=date.today(),
+                    position__term_to__gte=date.today(),
+                    status='appointed',
+                    applicant=contact_card.application.applicant
+                )
+            )
+        else:
+            self.fields['application'].queryset = Application.objects.filter(
+                position__term_from__lte=date.today(),
+                position__term_to__gte=date.today(),
+                status='appointed',
+                contact_card__isnull=True,
+            )
+
+
 @register_snippet
 class ContactCard(models.Model):
-    role = models.OneToOneField(
-        'Role',
+
+    application = models.OneToOneField(
+        'Application',
+        on_delete=models.CASCADE,
+        related_name='contact_card',
         blank=True,
-        null=True,
-        on_delete=models.SET_NULL,
-        related_name='contact_cards',
+        null=True
     )
+
     picture = models.ForeignKey(
         'wagtailimages.Image',
         null=True,
@@ -28,10 +65,12 @@ class ContactCard(models.Model):
         verbose_name=_('English Description'),
         blank=True,
     )
+
     description_sv = RichTextField(
         verbose_name=_('Swedish Description'),
         blank=True,
     )
+
     description = TranslatedField('description_en', 'description_sv')
 
     name = models.CharField(
@@ -48,6 +87,7 @@ class ContactCard(models.Model):
         help_text=_('Overrides role name from the selected role.'),
         blank=True,
     )
+
     role_text_sv = models.CharField(
         max_length=100,
         verbose_name=_('Swedish Role Name'),
@@ -64,16 +104,22 @@ class ContactCard(models.Model):
     )
 
     def __str__(self) -> str:
-        if self.role:
-            return self.role.__str__()
-        else:
-            return '%(name)s - %(role)s' % {
-                'name': self.name,
-                'role': self.role_text
+        if self.application:
+            return '%(teams)s | %(position)s - %(applicant)s' % {
+                'teams': self.application.position.role.team_names,
+                'position': self.application.position,
+                'applicant': self.application.applicant
             }
+        return '%(name)s - %(role)s' % {
+            'name': self.name,
+            'role': self.role_text
+        }
+
+    list_filter = ('application__position__role__team')
+    base_form_class = ContactBlockForm
 
     panels = [
-        FieldPanel('role'),
+        FieldPanel('application'),
         ImageChooserPanel('picture'),
         FieldPanel('description_en'),
         FieldPanel('description_sv'),
