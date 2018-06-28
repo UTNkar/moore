f5e457664aed5f630b461c3e5bc43cbe149fca0f
furedal <cfuredal@gmail.com>
2018-06-28 12:48:06 +0200

    refactor contact cards html to use css instead of inline styles

--- src/involvement/templatetags/involvement_tags.py
+++ src/involvement/templatetags/involvement_tags.py
@@ -3,25 +3,37 @@ from datetime import date
 from django import template
 from django.utils.translation import ugettext_lazy as _
 
+
 register = template.Library()
 
-
 @register.inclusion_tag('involvement/tags/contact_card.html')
-def contact_card(card, width=7):
-    role = {}
-    if card.role:
-        role['role_text'] = card.role.__str__()
-        member = card.role.in_role().first()
-        if member:
-            role['name'] = member.get_full_name()
-            role['email'] = member.get_primary_email()
-        else:
-            role['name'] = _('Vacant Position')
-    return {
-        'card': card,
-        'role': role,
-        'width': width,
+def contact_card(contact_card, width=7):
+
+    data = {
+        'width': width
     }
+
+    position = contact_card.position
+    role = position.role
+    application = contact_card.application
+
+    data['picture'] = contact_card.picture
+    data['title'] = role.name
+    data['description'] = role.description
+
+
+
+    if application is not None:
+        data['card_name'] = application.applicant.get_full_name()
+        data['phone_number'] = role.phone_number if role.phone_number else application.applicant.phone_number
+        data['email'] = role.contact_email if role.contact_email else application.applicant.email
+    else:
+        data['card_name'] = _('Vacant Position')
+        data['phone_number'] = role.phone_number
+        data['email'] = role.election_email
+
+
+    return data
 
 
 @register.filter
