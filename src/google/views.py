6a43ea96664619e77c60a4a25119b29d8c4aa25b
furedal <cfuredal@gmail.com>
2018-08-14 14:38:16 +0200

    Adds event-block containing, youtube, google calendar, instagram and facebook
    - Youtube and Google calendar uses googles V3 Api and requires a
    GOOGLE_API_KEY to be set in the environment variables
    
    - Instagram requires INSTAGRAM_ACCESS_TOKEN to be set in the
    environment variables. We require access_token instead of client_id and
    client_secret because we can’t authenticate on server-side. Instagrams
    access token doesn’t expire atm.
    
    - Facebook requires FACEBOOK_APP_ID to be set in the environment
    variables.

--- src/google/views.py
+++ src/google/views.py
@@ -1,3 +1,18 @@
-# from django.shortcuts import render
+from django.conf import settings
+from django.http import JsonResponse
+from datetime import datetime
+from dateutil.parser import parse
+from urllib.parse import quote
+import requests
+import json
 
-# Create your views here.
+from google.api import google_calendar_list_events
+
+
+def view_google_calendar(request, id):
+    params = {
+        'timeMin': request.GET.get('time_min'),
+        'timeMax': request.GET.get('time_max'),
+    }
+
+    return JsonResponse(google_calendar_list_events(id, params))
