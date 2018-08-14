d4d71294045e1e11089813421248af6c3dd2356d
furedal <cfuredal@gmail.com>
2018-08-14 14:40:25 +0200

    added placeholder for dev environment

--- src/moore/settings/dev.py
+++ src/moore/settings/dev.py
@@ -65,6 +65,15 @@ DEFAULT_FROM_EMAIL = 'info@localhost'
 MEMBERSHIP_API_USER = 'test'
 MEMBERSHIP_API_PASSWORD = 'test'
 
+# Google API
+GOOGLE_API_KEY = 'my_google_api_key'
+
+# Instagram API
+INSTAGRAM_ACCESS_TOKEN = 'my_instagram_access_token'
+
+# Facebook API
+FACEBOOK_APP_ID = 'my_facebook_app_id'
+
 try:
     from .local import *
 except ImportError:
