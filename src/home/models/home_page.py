8c2ab884534846fa521ce129fea1281bf0c6b741
Filip Gölander <filipgolander@gmail.com>
2019-08-31 22:01:17 +0200

    Searchbar setting (fix #305) (#306)
    * Correctly insert page introduction in forms (Fix #303)
    
    * Added setting to show or not show search-bar on the home-page.
    
    * Revert "Correctly insert page introduction in forms (Fix #303)"
    
    This reverts commit de200da2ee0653254eae0fcbc0bc3ece5d365497.
    
    * Change to adhere to pep8 style.
    
    * Fixes adherence to style-tests.
    Adds missing db migration files.

--- src/home/models/home_page.py
+++ src/home/models/home_page.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, unicode_literals
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from wagtail.admin.edit_handlers import InlinePanel, FieldPanel, \
-        StreamFieldPanel, TabbedInterface, ObjectList
+        StreamFieldPanel, TabbedInterface, ObjectList, MultiFieldPanel
 from wagtail.core.fields import StreamField
 from wagtail.core.models import Page
 from blocks.models import WAGTAIL_STATIC_BLOCKTYPES
@@ -14,15 +14,16 @@ class HomePage(Page):
     # ---- General Page information ------
     title_sv = models.CharField(max_length=255)
     translated_title = TranslatedField('title', 'title_sv')
+    show_searchbar = models.BooleanField(default=True)
 
     body_en = StreamField(
-        WAGTAIL_STATIC_BLOCKTYPES
-        + [('news', LatestNewsBlock())],
+        WAGTAIL_STATIC_BLOCKTYPES +
+        [('news', LatestNewsBlock())],
         blank=True,
     )
     body_sv = StreamField(
-        WAGTAIL_STATIC_BLOCKTYPES
-        + [('news', LatestNewsBlock())],
+        WAGTAIL_STATIC_BLOCKTYPES +
+        [('news', LatestNewsBlock())],
         blank=True,
     )
     body = TranslatedField('body_en', 'body_sv')
@@ -38,10 +39,16 @@ class HomePage(Page):
         StreamFieldPanel('body_sv'),
     ]
 
+    custom_settings_panel = Page.settings_panels + [
+        MultiFieldPanel([
+            FieldPanel('show_searchbar')
+        ], 'Searchbar settings')
+    ]
+
     edit_handler = TabbedInterface([
         ObjectList(banner_panels, heading=_('Banners')),
         ObjectList(content_panels_en, heading=_('English')),
         ObjectList(content_panels_sv, heading=_('Swedish')),
         ObjectList(Page.promote_panels, heading=_('Promote')),
-        ObjectList(Page.settings_panels, heading=_('Settings')),
+        ObjectList(custom_settings_panel, heading=_('Settings')),
     ])
