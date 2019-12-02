edaacced9e39805a6436cbb8b80e346b236ee4b6
furedal <cfuredal@gmail.com>
2019-12-02 16:55:05 +0100

    Add support to add translations that isn't compiled by makemessages

---
+++ src/members/django_extra_messages.py
@@ -0,0 +1,7 @@
+# Messages that normally isn't found using makemessages can be added here
+# to force Django to detect them
+
+_ = lambda s: s
+django_standard_messages_to_add = [
+    _("Username or SSN (YYYYMMDD-XXXX)"),
+]
