37cc4b1621e427a01662213bd1f0a9341eb4ac53
furedal <cfuredal@gmail.com>
2019-12-05 17:07:11 +0100

    Should fix so that member status is updated frequently

--- src/members/cron.py
+++ src/members/cron.py
@@ -1,31 +1,18 @@
 import kronos
-import requests
-from django.conf import settings
-from requests.auth import HTTPDigestAuth
-
+from datetime import date, timedelta
 from members.models import Member
 
 
+# There is now good way for bulk updates and
+# without bulk update we will need to make at least 2 requests against melos to
+# update one single member. To reduce the amounts of requests,
+# we will only update status for active members e.g the ones that has
+# status="member" as this value is updated when you log in to your account.
+# We also restrict the updates to every 3 days.
 @kronos.register('0 3 * * 1')  # At 03:00 on Monday.
 def update_membership_status():
-    r = requests.get(
-        'https://register.utn.se/api.php',
-        auth=HTTPDigestAuth(settings.MEMBERSHIP_API_USER,
-                            settings.MEMBERSHIP_API_PASSWORD),
-        params={
-            'action': 'list',
-        },
-    )
-    try:
-        data = r.json()
-    except ValueError:
-        return
-
-    for member in Member.objects.all():
-        if member.person_number().replace('-', '') in data:
-            member.update_status(data='member')
-        else:
-            member.update_status(data='nonmember')
-        Member.objects.filter(pk=member.pk).update(
-            status=member.status, status_changed=member.status_changed
-        )
+    for member in Member.objects.filter(
+                    status='member',
+                    status_changed__gte=date.today() - timedelta(days=3)
+                ):
+        member.update_status()
