9a79aa10b073d83ddf222b754d4cf7d4852e7430
Daniel Fehrm <daniel@fehrm.se>
2020-02-06 17:05:34 +0100

    Added t number as valid ssn

--- src/utils/validators.py
+++ src/utils/validators.py
@@ -6,10 +6,10 @@ class SSNValidator(validators.RegexValidator):
     def __init__(self):
         super(SSNValidator, self).__init__(
             # This regex makes sure that the year is either 1900 or 2000
-            regex=r'^[1-2][0|9][0-9]{2}[0-1][0-9][0-3][0-9][-][0-9]{4}$|'  # noqa: E501, YYYYMMDD-XXXX
-                  r'^[1-2][0|9][0-9]{2}[0-1][0-9][0-3][0-9][0-9]{4}$|'  # noqa: E501, YYYYMMDDXXXX
-                  r'^[0|9][0-9]{1}[0-1][0-9][0-3][0-9][-][0-9]{4}$|'  # noqa: E501, YYMMDD-XXXX
-                  r'^[0|9][0-9]{1}[0-1][0-9][0-3][0-9][0-9]{4}$',  # noqa: E501, YYMMDDXXXX
+            regex=r'^[1-2][0|9][0-9]{2}[0-1][0-9][0-3][0-9][-](T|t|[0-9])[0-9]{3}$|'  # noqa: E501, YYYYMMDD-XXXX
+                  r'^[1-2][0|9][0-9]{2}[0-1][0-9][0-3][0-9](T|t|[0-9])[0-9]{3}$|'  # noqa: E501, YYYYMMDDXXXX
+                  r'^[0|9][0-9]{1}[0-1][0-9][0-3][0-9][-](T|t|[0-9])[0-9]{3}$|'  # noqa: E501, YYMMDD-XXXX
+                  r'^[0|9][0-9]{1}[0-1][0-9][0-3][0-9](T|t|[0-9])[0-9]{3}$',  # noqa: E501, YYMMDDXXXX
             message=_(
                 'Use the format YYYYMMDD-XXXX, YYMMDD-XXXX, \
                 YYYYMMDDXXXX, YYMMDDXXXX for your ssn.'
