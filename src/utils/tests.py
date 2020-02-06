c91d2501f0f55a3174c5832bf6b26892cd3a4eac
Daniel Fehrm <daniel@fehrm.se>
2020-02-06 17:05:13 +0100

    Added tests for ssn validator

---
+++ src/utils/tests.py
@@ -0,0 +1,54 @@
+from django.test import TestCase
+from utils.validators import SSNValidator
+from django.core.exceptions import ValidationError
+
+
+class SSNValidatorTests(TestCase):
+    ssn_validator = SSNValidator()
+
+    def ssn_is_valid(self, ssn):
+        try:
+            self.ssn_validator(ssn)
+            return True
+        except ValidationError:
+            return False
+
+    def test_long_format_with_dash(self):
+        self.assertTrue(
+            self.ssn_is_valid("19330524-8585"),
+            "Valid SSN format failed"
+        )
+        self.assertTrue(
+            self.ssn_is_valid("19330524-T585"),
+            "Valid SSN format with T failed"
+        )
+
+    def test_long_format_with_no_dash(self):
+        self.assertTrue(
+            self.ssn_is_valid("193305248585"),
+            "Valid SSN format failed"
+        )
+        self.assertTrue(
+            self.ssn_is_valid("19330524T585"),
+            "Valid SSN format with T failed"
+        )
+
+    def test_short_format_with_dash(self):
+        self.assertTrue(
+            self.ssn_is_valid("330524-8585"),
+            "Valid SSN format failed"
+        )
+        self.assertTrue(
+            self.ssn_is_valid("330524-T585"),
+            "Valid SSN format with T failed"
+        )
+
+    def test_short_format_with_no_dash(self):
+        self.assertTrue(
+            self.ssn_is_valid("3305248585"),
+            "Valid SSN format failed"
+        )
+        self.assertTrue(
+            self.ssn_is_valid("330524T585"),
+            "Valid SSN format with T failed"
+        )
