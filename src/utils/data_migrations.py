e84bdfe4753db872a7da13c8caff425100e8afcb
Max Gabrielsson <max@gabrielsson.com>
2020-04-16 14:58:52 +0200

    Fixed section migration so that html and news blocks are kept (#407)
    * fixed section migration so that html and news blocks are kept
    
    * fixed flake8
    
    Co-authored-by: Daniel Fehrm <daniel@fehrm.se>

--- src/utils/data_migrations.py
+++ src/utils/data_migrations.py
@@ -31,6 +31,37 @@ def block_filter_map(block, block_type, mapper_func):
 
 
 """
+Recursively apply a filter operation on a block by traversing it
+and its children, and calling the predicate_func on each block. Only
+the blocks that make predicate_func return true are returned back, the
+others have their values cleared. Important: Does not deep copy by
+itself. Make sure to perform a deep copy beforehand!
+
+:param block: The block to apply the filter-map operation on
+:param predicate_func: Function that decides wether or not the block
+is filtered
+:return: Returns a block hierarchy only containing the passing blocks.
+"""
+
+
+def block_filter(block, predicate_func):
+    if isinstance(block, list):
+        for i in range(len(block)):
+            block[i] = block_filter(block[i], predicate_func)
+    elif isinstance(block, dict):
+        if 'type' in block:
+            # This is a proper block
+            if predicate_func(block):
+                for k, v in block.items():
+                    block[k] = block_filter(v, predicate_func)
+            else:
+                return {'type': block['type'], 'value': {}}
+
+    # int or string or something, just return untouched
+    return block
+
+
+"""
 Recursively apply a filter-map operation on a stream field,
 replacing every block or sub-block with the same type as block_type
 with the blocks produced by applying the mapper_func.
