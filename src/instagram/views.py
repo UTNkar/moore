99411f34f6b47a9e23ade531ce32782f30522ecb
Daniel Fehrm <daniel@fehrm.se>
2020-08-28 20:30:28 +0200

    Instagram feeds are created when getting request from instagram

--- src/instagram/views.py
+++ src/instagram/views.py
@@ -1,14 +1,22 @@
 from instagram.utils import InstagramUtils
-from django.http import HttpResponse
+from instagram.models import InstagramFeed
+from django.shortcuts import redirect
+import datetime
+from django.utils import timezone
 
 
 def get_code(request):
     code = request.GET.get("code")
-    image_dict = InstagramUtils.get_latest_image(code)
+    access_token, user_id, expires_in = \
+        InstagramUtils.get_long_lived_token(code)
 
-    return HttpResponse(
-        "<p>" + image_dict.get("username") + "</p>" +
-        "<a href='" + image_dict.get("permalink") + "'>" +
-        "<img src='" + image_dict.get("media_url") + "'/>" +
-        "</a>"
+    expires = timezone.now() + datetime.timedelta(seconds=expires_in)
+
+    feed = InstagramFeed.objects.create(
+        access_token=access_token,
+        expires=expires
     )
+
+    feed.save()
+
+    return redirect("/admin/instagram/instagramfeed/")
