8fb9c80ac9e0ad0fa4dfd249874bbc639f3ddbc4
Daniel Fehrm <daniel@fehrm.se>
2020-08-29 19:46:19 +0200

    added error handling for instagram authentication

--- src/instagram/views.py
+++ src/instagram/views.py
@@ -5,6 +5,7 @@ import datetime
 from django.utils import timezone
 from django.contrib import messages
 from django.utils.translation import ugettext_lazy as _
+# from sentry_sdk import capture_exception
 
 
 def code_from_instagram(request):
@@ -14,17 +15,36 @@ def code_from_instagram(request):
     This code is then used to retreive a long lived token.
     """
     code = request.GET.get("code")
-    access_token, user_id, expires_in = \
-        InstagramUtils.get_long_lived_token(code)
+    if not code:
+        error_reason = request.GET.get("error_reason")
 
-    expires = timezone.now() + datetime.timedelta(seconds=expires_in)
-    account_name = InstagramUtils.get_account_name(access_token)
+        if error_reason == "user_denied":
+            error_message = _(
+                "Could not add your Instagram account since you denied access"
+            )
+        else:
+            error_message = _(
+                "Could not add your Instagram account because of an error"
+            )
+            # capture_exception(
+            #     Exception("An error occured with the request from Instagram")
+            # )
 
-    InstagramFeed.objects.update_or_create(
-        access_token=access_token,
-        expires=expires,
-        account_name=account_name
-    )
+        messages.error(request, error_message)
 
-    messages.success(request, _("Your Instagram account was added"))
+    else:
+        access_token, expires_in = \
+            InstagramUtils.get_long_lived_token(code)
+
+        expires = timezone.now() + datetime.timedelta(seconds=expires_in)
+        account_name = InstagramUtils.get_account_name(access_token)
+
+        InstagramFeed.objects.update_or_create(
+            access_token=access_token,
+            expires=expires,
+            account_name=account_name
+        )
+
+        messages.success(request, _("Your Instagram account was added"))
+
     return redirect("/admin/instagram/instagramfeed/")
