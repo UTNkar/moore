e9000f0c6e4093ff1e7e9ef9daa8565d5813049d
Daniel Fehrm <daniel@fehrm.se>
2020-08-29 14:40:59 +0200

    use update or create

--- src/instagram/views.py
+++ src/instagram/views.py
@@ -15,17 +15,11 @@ def get_code(request):
     expires = timezone.now() + datetime.timedelta(seconds=expires_in)
     account_name = InstagramUtils.get_account_name(access_token)
 
-    try:
-        feed = InstagramFeed.objects.get(pk=account_name)
-        feed.access_token = access_token
-        feed.expires = expires
-    except InstagramFeed.DoesNotExist:
-        feed = InstagramFeed.objects.create(
-            access_token=access_token,
-            expires=expires,
-            account_name=account_name
-        )
-    finally:
-        feed.save()
-        messages.success(request, _("Your Instagram account was added"))
-        return redirect("/admin/instagram/instagramfeed/")
+    InstagramFeed.objects.update_or_create(
+        access_token=access_token,
+        expires=expires,
+        account_name=account_name
+    )
+
+    messages.success(request, _("Your Instagram account was added"))
+    return redirect("/admin/instagram/instagramfeed/")
