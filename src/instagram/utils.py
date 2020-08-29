191d9f02cc0aeb724783d7b027ecab1009c4db68
Daniel Fehrm <daniel@fehrm.se>
2020-08-29 15:41:44 +0200

    added documentation for instagram utils

--- src/instagram/utils.py
+++ src/instagram/utils.py
@@ -1,4 +1,5 @@
 import requests
+from typing import Tuple, Dict
 from django.conf import settings
 from django.utils.http import urlencode
 from instagram.models import InstagramFeed
@@ -7,18 +8,22 @@ from django.utils import timezone
 
 
 class InstagramUtils():
+    """
+    Utilities for the Instagram basic display API.
+    https://developers.facebook.com/docs/instagram-basic-display-api/
+    """
     _redirect_url = settings.INSTAGRAM_REDIRECT_URL
     _app_id = settings.INSTAGRAM_APP_ID
     _app_secret = settings.INSTAGRAM_APP_SECRET
-    _code = ''
     _api_base_url = "https://api.instagram.com"
     _graph_base_url = "https://graph.instagram.com"
 
-    def __init__(self, params_from_instagram):
-        self._code = params_from_instagram["code"]
-
     @staticmethod
-    def get_authorization_url():
+    def get_authorization_url() -> str:
+        """
+        Returns the url to Instagram that users has to click to
+        authenticate their account.
+        """
         get_vars = {
             'app_id': InstagramUtils._app_id,
             'redirect_uri': InstagramUtils._redirect_url,
@@ -31,7 +36,19 @@ class InstagramUtils():
             urlencode(get_vars)
 
     @staticmethod
-    def get_short_lived_token(instagram_code):
+    def _get_short_lived_token(instagram_code: str) -> str:
+        """
+        Gets a short lived token from instagram with the code
+        received from instagram.
+
+        Parameters:
+
+        instagram_code: The code received from instagram
+
+        Returns:
+
+        The short lived access token from Instagram
+        """
         post_params = {
             'client_id': InstagramUtils._app_id,
             'redirect_uri': InstagramUtils._redirect_url,
@@ -47,15 +64,24 @@ class InstagramUtils():
 
         decoded_response = response.json()
 
-        return (
-            decoded_response.get("access_token"),
-            decoded_response.get("user_id")
-        )
+        return decoded_response.get("access_token"),
 
     @staticmethod
-    def get_long_lived_token(instagram_code):
-        short_lived_token, user_id = \
-            InstagramUtils.get_short_lived_token(instagram_code)
+    def get_long_lived_token(instagram_code: str) -> Tuple[str, int]:
+        """
+        Gets a long lived access token with the code from Instagram.
+
+        Parameters:
+
+        instagram_code: The code received from instagram
+
+        Returns:
+
+        A tuple with the access token and the number of seconds until it
+        expires (access_token, expires_in)
+        """
+        short_lived_token = \
+            InstagramUtils._get_short_lived_token(instagram_code)
 
         get_params = {
             'grant_type': 'ig_exchange_token',
@@ -72,12 +98,25 @@ class InstagramUtils():
 
         return (
             decoded_response.get("access_token"),
-            user_id,
             decoded_response.get("expires_in")
         )
 
     @staticmethod
-    def get_latest_media(account_name):
+    def get_latest_media(account_name: str) -> Dict:
+        """
+        Gets the latest media from Instagram for the specified account.
+        The account must exist in the database and have a vaild access token.
+
+        Parameters:
+
+        account_name: The name of the account.
+
+        Returns:
+
+        A dict with the fields permalink, media_url, media_type, thumbnail_url.
+        More details about them are found here:
+        https://developers.facebook.com/docs/instagram-basic-display-api/reference/media#fields
+        """
         account = InstagramFeed.objects.get(pk=account_name)
 
         get_params = {
@@ -97,7 +136,19 @@ class InstagramUtils():
         return first_image_dict
 
     @staticmethod
-    def get_account_name(access_token):
+    def get_account_name(access_token: str) -> str:
+        """
+        Gets the name of the account with the associated access_token
+        from Instagram.
+
+        Parameters:
+
+        access_token: The access token for the account
+
+        Returns:
+
+        The name of the Instagram account
+        """
         get_params = {
             "fields": "username",
             "access_token": access_token,
@@ -114,6 +165,12 @@ class InstagramUtils():
 
     @staticmethod
     def renew_long_lived_tokens():
+        """
+        Refreshes all long lived tokens that will expire in 10 days or earlier.
+
+        More info:
+        https://developers.facebook.com/docs/instagram-basic-display-api/guides/long-lived-access-tokens#refresh-a-long-lived-token
+        """
         in_ten_days = timezone.now() + datetime.timedelta(days=10)
         feeds_to_renew = InstagramFeed.objects.filter(expires__lte=in_ten_days)
 
