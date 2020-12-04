6bce97ad9450b62edd38435e225a3a1afb4e71bb
Daniel Fehrm <daniel@fehrm.se>
2020-12-04 14:36:54 +0100

    Rebuild the header (#512)
    * disable navbar from materialize
    
    * Remade template files for structure header
    
    * redesigned structure header
    
    * start rebuild of navigation by fixing logo
    
    * Move mobile navigation bar to separate file
    
    * added spacing to structure header
    
    * dont add li tags on top level links in menu
    
    * use em instead of pixels in structure header
    
    * rebuild the navigation bar
    
    * fix minor errors
    
    * fix spelling

--- src/moore/static/js/navigation.js
+++ src/moore/static/js/navigation.js
@@ -3,4 +3,27 @@ $(document).ready(function(){
     $(".dropdown-button").dropdown({
         hover: true
     });
+
+    /**
+     * Moves the dropdown menu in the navigation bar on hover
+     * if the user has scrolled in the navigation bar. This behaviour
+     * does not exist in materialize so it had to be added manually.
+     */
+    $("nav .main-menu > .dropdown-button").hover(function() {
+        const elementToActivateId = this.dataset.activates;
+        const elementToActivate = $(`#${elementToActivateId}`)
+
+        const dropDownLeft = parseInt(elementToActivate.css("left"));
+        const buttonLeft = Math.floor($(this).offset().left);
+        const currentScrollLeft = $("nav .main-menu").scrollLeft();
+
+        if (buttonLeft == dropDownLeft - currentScrollLeft){
+            elementToActivate.css("left", dropDownLeft - currentScrollLeft)
+        }
+    }, function(){
+        /*
+         * This empty function is needed so that the above function
+         * doesn't get runned on hover in and hover out
+         */
+    });
 });
