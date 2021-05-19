db5d622221298c02f78bd96ba8ab2f69e53a3d9f
Daniel Fehrm <daniel@fehrm.se>
2021-05-19 18:02:13 +0200

    forgot import in production

--- src/moore/settings/production.py
+++ src/moore/settings/production.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 from __future__ import absolute_import, unicode_literals
 import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 from .base import *
 
