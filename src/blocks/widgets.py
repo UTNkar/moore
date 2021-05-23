f310d41f7cc7af7b633a548633078e640361f180
Daniel Fehrm <daniel@fehrm.se>
2021-05-23 19:55:34 +0200

    Fix html not loading default value

--- src/blocks/widgets.py
+++ src/blocks/widgets.py
@@ -6,26 +6,30 @@ class CodeMirrorWidget(WidgetWithScript, forms.Textarea):
 
     def render_js_init(self, id, name, value):
         js = """
-        CodeMirror.fromTextArea(
-        document.getElementById("{id}"),
-        {{
-        lineWrapping: true,
-        indentUnit: 4,
-        mode: "htmlmixed",
-        autoRefresh: true
-        }}
-        );
+        document.addEventListener('DOMContentLoaded', function(){{
+            CodeMirror.fromTextArea(
+                document.getElementById("{id}"),
+                {{
+                lineWrapping: true,
+                indentUnit: 4,
+                mode: "htmlmixed",
+                autoRefresh: true
+                }}
+            )
+        }});
         """
-        return js.format(id=id)
+        return js.format(id=id, value=value)
 
     @property
     def media(self):
         return forms.Media(
             css={'all': ('libraries/codemirror/codemirror.css',)},
-            js=('libraries/codemirror/codemirror.js',
+            js=(
+                'libraries/codemirror/codemirror.js',
                 'libraries/codemirror/autorefresh.js',
                 'libraries/codemirror/xml.js',
                 'libraries/codemirror/css.js',
                 'libraries/codemirror/javascript.js',
-                'libraries/codemirror/htmlmixed.js')
+                'libraries/codemirror/htmlmixed.js',
+            )
         )
