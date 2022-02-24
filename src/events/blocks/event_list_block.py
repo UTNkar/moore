71d1124d99bdfb9d5734ce822307fa6ce82ff7e3
Linus Shoravi <linusshoravi@gmail.com>
2022-02-24 19:54:41 +0100

    Fixes
    - Make it clearer when users haven't recieved a ticket for an event
    
    - Order participants by email.
    
    - Make sure instances are saved when 'ready_to_pay' is pressed.

--- src/events/blocks/event_list_block.py
+++ src/events/blocks/event_list_block.py
@@ -1,7 +1,7 @@
 from django.utils.translation import gettext_lazy as _
 from wagtail.snippets.blocks import SnippetChooserBlock
 from wagtail.core import blocks
-
+from events.models import Ticket
 
 class EventBlock(SnippetChooserBlock):
     def __init__(self, **kwargs):
@@ -18,6 +18,14 @@ class EventBlock(SnippetChooserBlock):
 
 class EventListBlock(blocks.StructBlock):
     events = blocks.ListBlock(EventBlock)
+
+    def get_context(self, value, parent_context=None):
+        ctx = super().get_context(value, parent_context=parent_context)
+
+        for event in ctx['self']['events']:
+            event.user_has_ticket = Ticket.objects.filter(owner=ctx['user'], event=event).exists()
+
+        return ctx
 
     class Meta:
         label = _('Event list')
