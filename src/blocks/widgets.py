6c4e5a3856abfc73141a3ddbc7133b68b01d8b3a
Daniel Fehrm <daniel@fehrm.se>
2023-04-07 00:35:22 +0200

    make codemirrorwidget render use super

--- src/blocks/widgets.py
+++ src/blocks/widgets.py
@@ -1,7 +1,6 @@
 from django import forms
 from wagtail.utils.widgets import WidgetWithScript
 from django.utils.html import format_html
-from django.utils.safestring import mark_safe
 from string import Template
 from django.forms.widgets import Media
 
@@ -29,8 +28,9 @@ class CodeMirrorWidget(WidgetWithScript, forms.Textarea):
 
     def render_js_init(self, id, name, value):
         js = Template("""
-        //document.addEventListener('DOMContentLoaded', function(){{
-            import {EditorState, EditorView, keymap, defaultKeymap} from '/static/my-lib.js';
+            import {
+                EditorState, EditorView, keymap, defaultKeymap
+            } from '/static/codemirror.js';
             let startState = EditorState.create({
                 doc: "Hello World",
                 extensions: [keymap.of(defaultKeymap)],
@@ -40,26 +40,20 @@ class CodeMirrorWidget(WidgetWithScript, forms.Textarea):
                 state: startState,
                 parent: document.querySelector("#$id"),
             });
-            console.log(document.querySelector("#$id"))
-        //}});
+            console.log(document.querySelector("#$id"));
         """)
         return js.substitute(id=id)
 
     def render(self, name, value, attrs=None, renderer=None):
-        # no point trying to come up with sensible semantics for when 'id' is missing from attrs,
-        # so let's make sure it fails early in the process
-        try:
-            id_ = attrs['id']
-        except (KeyError, TypeError):
-            raise TypeError(
-                "WidgetWithScript cannot be rendered without an 'id' attribute")
-
-        value_data = self.get_value_data(value)
-        widget_html = self.render_html(name, value_data, attrs)
-
-        js = self.render_js_init(id_, name, value_data)
-        out = '{0}<script type="module">{1}</script>'.format(widget_html, js)
-        return mark_safe(out)
+        # Javascript in wagtail don't run as type=module and as of wagtail 2.16
+        # there is no support for this. There for we have to override it.
+        # render returns the tempalte and the extra script tag, all we have to
+        # do is to add type=module ourselves
+        out = super().render(
+            name, value, attrs=attrs, renderer=renderer
+        )
+        out = out.replace("<script>", "<script type='module'>")
+        return out
 
     @property
     def media(self):
