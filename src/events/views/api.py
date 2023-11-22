26cc620f125b31999e21817cc9f661886b615ff5
Ankur <ankur_roy@outlook.com>
2023-11-22 17:56:59 +0100

    Changed to authenticated or readonly

--- src/events/views/api.py
+++ src/events/views/api.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
 from events.serializers.serializers import *
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from events.models.costs import Costs
 from events.models.event import Event
 from events.models.participant import Participant
@@ -9,26 +9,26 @@ from events.models.ticket import Ticket
 
 class CostsViewSet(viewsets.ModelViewSet):
     serializer_class = CostsSerializer
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Costs.objects.all()
 
 class EventViewSet(viewsets.ModelViewSet):
     serializer_class = EventSerializer
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Event.objects.all()
 
 class ParticipantViewSet(viewsets.ModelViewSet):
     serializer_class = ParticipantSerializer
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Participant.objects.all()
 
 class EventApplicationViewSet(viewsets.ModelViewSet):
     serializer_class = EventApplicationSerializer
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = EventApplication.objects.all()
 
 class TicketViewSet(viewsets.ModelViewSet):
     serializer_class = TicketSerializer
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Ticket.objects.all()
 
--- src/involvement/views/position_api.py
+++ src/involvement/views/position_api.py
@@ -1,14 +1,14 @@
 from rest_framework import viewsets
 from involvement.serializers.position_serializer import PositionSerializer, PositionDepthSerializer
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from involvement.models.position import Position
 
 class PositionViewSet(viewsets.ModelViewSet):
     serializer_class = PositionSerializer
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Position.objects.all()
 
 class Position2ViewSet(viewsets.ModelViewSet):
     serializer_class = PositionDepthSerializer
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Position.objects.all()
