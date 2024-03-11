0ae16148dc5032739cc1fa775140596fb8dec46a
Ludvig Aldén <30798446+ludvigalden@users.noreply.github.com>
2024-03-11 15:53:51 +0100

    ensure HTTP_HOST is correctly defined (#807)
    * ensure HTTP_HOST is correctly defined
    
    * remove print
    
    * add even more fallbacks
    
    * change admin host urlpattern order in prod
    
    * do not alter host if already correct
    
    * fix key error

--- src/admin/urls.py
+++ src/admin/urls.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import, unicode_literals
 
+from django.conf import settings
 from django.conf.urls import include, url
 
 from wagtail.admin import urls as wagtailadmin_urls
@@ -10,7 +11,14 @@ from moore.urls import urlpatterns as base_urlpatterns
 urlpatterns = base_urlpatterns.copy()
 
 # Insert `wagtailadmin_urls` at appropriate level
-urlpatterns.insert(
-    len(urlpatterns) - 3,
-    url(r'', include(wagtailadmin_urls))
-)
+if settings.DEBUG:
+    # If DEBUG, two static file-urls are put at the end
+    urlpatterns.insert(
+        len(urlpatterns) - 3,
+        url(r'', include(wagtailadmin_urls))
+    )
+else:
+    urlpatterns.insert(
+        len(urlpatterns) - 1,
+        url(r'', include(wagtailadmin_urls))
+    )
